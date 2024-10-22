355de1c01853d71fca5b3e877cb29f547a00c0f2
gabrielfreitas1406 <gabriel.freitas@icomp.ufam.edu.br>
2024-10-22 16:13:24 -0400

    :zap: [PERF]: Colocando opções na navbar

--- frontend/src/app/components/HomePage.tsx
+++ frontend/src/app/components/HomePage.tsx
@@ -7,7 +7,7 @@ import Recommendations from "./Recommendations";
 const HomePage: React.FC = () => {
   const headerProps = {
     logo: "https://cdn.builder.io/api/v1/image/assets/TEMP/d0e794bf66420096e920bd50e32de1115938e28c4c8628a18e959cf647782974?placeholderIfAbsent=true&apiKey=83976f5dcdaa41408687cac3bc676028",
-    navItems: ["aaaaa", "aaaa", "aaaaa"],
+    navItems: ["Encontrar recomendação", "Contato", "Sobre"],
   };
 
   return (
