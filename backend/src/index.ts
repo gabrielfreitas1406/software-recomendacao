faffcd1ecebd976d3799cfaf8f4127ee91d236d6
gabrielfreitas1406 <gabriel.freitas@icomp.ufam.edu.br>
2024-11-05 20:22:08 -0400

    :sparkles: [FEAT]: Configurando o cors e axios para comunicação do back com o front

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,20 @@ import express, { Request, Response } from "express";
 import validateEnv from "./utils/validadeEnv";
 import dotenv from "dotenv";
 import router from "./router/v1Router";
+import cors from "cors";
 
 dotenv.config();
 validateEnv();
-const PORT = process.env.PORT || 3333;
 
 const app = express();
+const PORT = process.env.PORT || 3333;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(router);
