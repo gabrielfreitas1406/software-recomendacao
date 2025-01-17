c42f25887bbf1b2234b8895f25dcbf70b18886fa
gabrielfreitas1406 <gabriel.freitas@icomp.ufam.edu.br>
2025-01-17 16:38:36 -0400

    :sparkles: [FEAT]: Ajustando a lista de recomendações

--- frontend/src/app/components/card/ListRecursos/ListRecursos.tsx
+++ frontend/src/app/components/card/ListRecursos/ListRecursos.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import { ListRecursosProps } from "../../../types/resultRecommenTypes";
 
 export const ListRecursos: React.FC<ListRecursosProps> = ({ text }) => (
-  <li className="feature-item">{text}</li>
+  <div className="feature-card">{text}</div>
 );
