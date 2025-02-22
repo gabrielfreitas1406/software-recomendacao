71b527ebd6f33d2d3fa5d83de3539b4ec6e22391
gabrielfreitas1406 <gabriel.freitas@icomp.ufam.edu.br>
2025-02-21 20:31:20 -0400

    :art: [STYLE]: Ajustando o componente da barra de pesquisa

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { HeaderActionButtons } from "./HeaderActionButtons";
+import BarraDeBusca from "./barraDeBusca/BarraDeBusca";
 
 interface HeaderProps {
   logo: string;
@@ -26,13 +27,7 @@ const Header: React.FC<HeaderProps> = ({ logo, navItems }) => {
           </a>
         ))}
       </nav>
-      <div className="search-container">
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/c69ed815b6be9bae5bef4073f1b1e0e843a6f946e06aa6450788588d87373cd5?placeholderIfAbsent=true&apiKey=83976f5dcdaa41408687cac3bc676028"
-          alt=""
-          className="search-icon"
-        />
-      </div>
+      <BarraDeBusca />
       <div className="auth-buttons">
         {/*<button className="auth-button register">Cadastro</button>
         <button className="auth-button login">Login</button>*/}
