3cdf5ff5d2eb90a2d3f528147ebce6f911ea093d
gabrielfreitas1406 <gabriel.freitas@icomp.ufam.edu.br>
2025-03-17 12:43:04 -0400

    :art: [STYLE]: Ajustando a organização da navbar

--- frontend/src/app/components/barraDeBusca/BarraDeBusca.tsx
+++ frontend/src/app/components/barraDeBusca/BarraDeBusca.tsx
@@ -11,7 +11,7 @@ export default function BarraDeBusca() {
   return (
     <Paper
       component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 600 }}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
